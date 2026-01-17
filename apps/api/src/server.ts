1262819b50688bc1d0fa1f576ff88edeb760e93d
uchiha-vivek <vs160125@gmail.com>
2026-01-18 02:41:49 +0530

    fix: made provision for env variable in api folder

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,12 +1,14 @@
-import 'dotenv/config'
+if (process.env.NODE_ENV !== 'production') {
+  await import('dotenv/config')
+}
 import { serve } from '@hono/node-server'
 import { app } from "./app.js"
 
-
+const port = Number(process.env.PORT) || 3001
 
 serve({
   fetch: app.fetch,
-  port: 3001,
+  port,
 })
 
-console.log('API running on http://localhost:3001')
+console.log(`API running on ${port}`)
